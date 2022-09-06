de8126c131752f738be21ca8c54acdb50245fcd6
crismix12 <91161880+crismix12@users.noreply.github.com>
2022-09-06 00:12:59 -0400

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -33,51 +33,12 @@ const WeatherCard = () => {
         axios
         .get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=31fb8580c76fe83c63502c5bfebcb631`)
             .then(res => {
-                setApiResponse(res.data)
-                // setApiTime(shiftSecconds(res.timezone))
-                shiftSecconds(res.timezone)
-                // shiftSecconds(res.timezone)
-            
+                setApiResponse(res.data)       
             })
-            // .finally((resx) => shiftSecconds(resx.timezone))
       };
   
       }, [])
   
-    console.log(apiResponse);
-
-    const shiftSecconds = (timezone => {
-        // let d = new Date((new Date().getTime())-(timezone + 14404)*1000);
-        // const timezoneOffset = (new Date()).getTimezoneOffset();
-        let d = new Date((new Date().getTime())-(timezone)*1000);
-        // let d = Date.now()  + 1000 * timezone;
-        // d.toISOString();
-        // d.toLocaleString('en-US', { timeZone: 'America/La_Paz' });
-
-        // console.log(d);
-        // return setApiTime(d);
-        // setApiTime(d);
-        setApiTime(d);
-        // return d;
-    })   
-    // const shiftSecconds = (() => {
-    //     // let d = new Date((new Date().getTime())-(timezone + 14404)*1000);
-    //     apiResponse.timezone = new Date((new Date().getTime())-timezone*1000);
-    //     // let d = Date.now()  + 1000 * timezone;
-    //     // d.toISOString();
-    //     apiResponse.timezone.toLocaleString('en-US', { timeZone: 'America/La_Paz' });
-
-    //     // console.log(d);
-    //     // return setApiTime(d);
-    // })   
-
-    shiftSecconds(apiResponse.timezone)
-
-    // shiftSecconds();
-
-    
-
-    // console.log(timezoneOffset);    
     
     return (
         <div className='weatherCard'>
