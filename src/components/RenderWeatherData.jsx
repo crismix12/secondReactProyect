1f04199f410594ba15f6190053a84431fb2b2714
crismix12 <91161880+crismix12@users.noreply.github.com>
2022-10-15 16:36:11 -0400

    Update RenderWeatherData.jsx

--- src/components/RenderWeatherData.jsx
+++ src/components/RenderWeatherData.jsx
@@ -13,15 +13,15 @@ const RenderWeatherData = ({city, country, icon, description, weatherId, humidit
     const changeBackground = (weatherId) => {
         if(weatherId === "800"){
             setBackground('https://lh3.googleusercontent.com/CnHg3skxcIhFKh5oE_ZV61x-a-tqWKIWC04a4hWkmQymuBRGlp3Kgnr_d3bEj-jgvPZAM1kh4nkpALUr0bDaUJdzPQ=w640-h400-e365-rj-sc0x00ffffff')
-        }else if(weatherId[0] ==="8"){
+        }else if(weatherId?.[0] ==="8"){
             setBackground('https://img.freepik.com/free-photo/scattered-clouds-blue-sky_23-2148824961.jpg?w=2000')
-        }else if(weatherId[0] ==="5"){
+        }else if(weatherId?.[0] ==="5"){
             setBackground("https://i.pinimg.com/originals/20/0e/08/200e0847167f87449c866fb2910b0b14.jpg")
-        }else if(weatherId[0] === "2"){
+        }else if(weatherId?.[0] === "2"){
             setBackground("https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/08/16467777883078.jpg")
-        }else if(weatherId[0] === "6"){
+        }else if(weatherId?.[0] === "6"){
             setBackground("https://static.seetheworld.com/image_uploader/photos/14/original/great-pre-season-snow-conditions-in-curchevel-courchevel-valley.jpg")
-        }else if(weatherId[0] === "7"){
+        }else if(weatherId?.[0] === "7"){
             setBackground("https://static.diffen.com/uploadz/2/24/Mist-1.jpg")
         }                
     }
